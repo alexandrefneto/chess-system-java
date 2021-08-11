c064bcfb9d153a22a2e5ce9cc58a2298cf00192b
alexandrefneto <alexandrefneto@hotmail.com>
2021-08-11 10:05:30 -0300

    ChessException and ChessPosition

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -25,11 +25,15 @@ public class ChessMatch {
 		return mat; // retorna a matriz de pecas da minha partida de xadrez
 	}
 	
+	private void placeNewPiece(char column, int row, ChessPiece piece) {
+		board.placePiece(piece, new ChessPosition(column, row).toPosition());
+	}
+	
 	private void initialSetup() {
 		// vai ser responsavel por iniciar a partida, colocando as pecas no tabuleiro
-		board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
-		board.placePiece(new King(board, Color.BLACK), new Position(0, 4));
-		board.placePiece(new King(board, Color.WHITE), new Position(7, 4));
+		placeNewPiece('b', 6, new Rook(board, Color.WHITE));
+		placeNewPiece('e', 8, new King(board, Color.BLACK));
+		placeNewPiece('e', 1, new King(board, Color.WHITE));
 	}
 	
 }
